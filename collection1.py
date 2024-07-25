347ac57594852b24fb52b7c00c331bc07ff8d3cf
Harshtrivedi456 <152944798+Harshtrivedi456@users.noreply.github.com>
2024-07-25 14:37:13 +0530

    Add files via upload

---
+++ collection1.py
@@ -0,0 +1,53 @@
+list1=[123,567,89] 
+print(list1)
+
+list2=["hello","how","are"] 
+print(list2)
+
+list3= ["hey",1223,"hello"] 
+print(list3)
+
+list1=["apple","mango",123,345]
+list2 = ["grapes"]
+print(list1+ list2)
+
+tup=(1,2,3,4)
+print(tup[0])
+
+my_tuple = (1, 2, 3, 4, 5)
+t1 = (2,3,4)
+t2 = (5,6,7)
+print(t1+t2)
+
+
+
+
+
+dict1={"comp": "computer" , "sci" : "science"}
+print(dict["comp"])
+dict2={"123":"computer","456" : "maths"}
+print(dict2["123"])
+print(dict1["comp"]+ dict2["123"])
+z={**dict1,**dict2}
+print(z)
+
+#print(dict1, dict2)
+print(dict1["comp"]+dict2["456"])
+
+
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+#print(set1 + set2)
+
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print(set1 + set2)
